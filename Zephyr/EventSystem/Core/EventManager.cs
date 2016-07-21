b6ac648b9b3f76475f2a0f091647632176d82a05
Denny Scott <Dennyscott301@gmail.com>
2016-07-20 23:03:55 -0500

    Initial Zephyr

---
+++ Zephyr/EventSystem/Core/EventManager.cs
@@ -0,0 +1,32 @@
+﻿using Zephyr.MonoBehaviours;
+
+namespace Zephyr.EventSystem.Core
+{
+    public class EventManager : AdvancedMonoBehaviour
+    {
+        public bool LimitQueueProcesing = false;
+        public float QueueProcessTime = 0.0f;
+
+        private static EventManagerModel _instance;
+
+
+        public static EventManagerModel Instance
+        {
+            get { return _instance ?? (_instance = new EventManagerModel()); }
+        }
+
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+            if (_instance == null) return;
+
+            _instance.ClearAll();
+            _instance = null;
+        }
+
+        private void Update()
+        {
+            Instance.ProcessEvents();
+        }
+    }
+}
